d24a2d8fd42608ba4ff39af6465f273a64e9087b
jordanjob <jordanjob@126.com>
2020-03-21 08:55:35 +0800

    Make the class1 inherited from Form

--- test_git/Class1.cs
+++ test_git/Class1.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace test_git
 {
-    class Class1
+    class Class1 : Form
     {
     }
 }
